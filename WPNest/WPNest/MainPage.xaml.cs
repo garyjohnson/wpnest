42c850c78c5c4924490dcee0f8043d9b17318620
Gary Johnson <gary@gjtt.com>
2012-11-07 01:31:16 -0500

    Proof of concept

---
+++ WPNest/WPNest/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows;
+using Microsoft.Phone.Controls;
+
+namespace WPNest {
+
+	public partial class MainPage : PhoneApplicationPage {
+
+		public MainPage() {
+			InitializeComponent();
+		}
+
+		private MainPageViewModel ViewModel {
+			get { return DataContext as MainPageViewModel; }
+		}
+
+		private void ButtonBase_OnClick(object sender, RoutedEventArgs e) {
+			ViewModel.Login();
+		}
+
+		private void OnUpClick(object sender, RoutedEventArgs e) {
+			ViewModel.Up();
+		}
+
+		private void OnDownClick(object sender, RoutedEventArgs e) {
+			ViewModel.Down();
+		}
+
+		private void OnGetInfoClick(object sender, RoutedEventArgs e) {
+			ViewModel.GetInfo();
+		}
+	}
+}
