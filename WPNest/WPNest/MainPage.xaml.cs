300a0e5ac9e9ae1b97f0e8dacd9c46f1c3da1b59
Gary Johnson <gary@gjtt.com>
2012-11-11 18:44:59 -0500

    Drawing ticks on thermostat

--- WPNest/WPNest/MainPage.xaml.cs
+++ WPNest/WPNest/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Media;
 using Microsoft.Phone.Controls;
 
 namespace WPNest {
@@ -16,6 +17,7 @@ namespace WPNest {
 		}
 
 		private async void OnLoaded(object sender, RoutedEventArgs e) {
+			DrawTicks();
 			await ViewModel.InitializeAsync();
 		}
 
@@ -27,5 +29,39 @@ namespace WPNest {
 			await ViewModel.LowerTemperatureAsync();
 		}
 
+		private void DrawTicks() {
+			PathGeometry ticksGeometry = new PathGeometry();
+
+			var rotateTransform = new RotateTransform();
+			rotateTransform.CenterX = orb.ActualWidth / 2;
+			rotateTransform.CenterY = orb.ActualHeight / 2;
+
+			Point start = new Point(orb.ActualWidth / 2, 20);
+			Point end = new Point(orb.ActualWidth / 2, 50);
+
+			Point newStart;
+			Point newEnd;
+
+			for (double i = 140.0d; i > -140.0d; i-=2.5d) {
+				rotateTransform.Angle = i;
+				newStart = rotateTransform.Transform(start);
+				newEnd = rotateTransform.Transform(end);
+				var tickFigure = GetPathFigure(newStart, newEnd);
+				ticksGeometry.Figures.Add(tickFigure);
+			}
+
+			ticks.Data = ticksGeometry;
+		}
+
+		private PathFigure GetPathFigure(Point start, Point end) {
+			PathFigure tickFigure = new PathFigure();
+			tickFigure.StartPoint = start;
+
+			LineSegment endOfTick = new LineSegment();
+			endOfTick.Point = end;
+
+			tickFigure.Segments.Add(endOfTick);
+			return tickFigure;
+		}
 	}
 }
