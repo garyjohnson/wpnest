4b8af8633811a0f89f0a8428db9e57bb7acaf046
Gary Johnson <gary@gjtt.com>
2013-08-03 11:59:46 -0400

    Adding new 1.5 binaries for release to marketplace

--- WPNest/WPNest/MainPage/NestSampleViewModel.cs
+++ WPNest/WPNest/MainPage/NestSampleViewModel.cs
@@ -6,26 +6,55 @@ namespace WPNest {
 	public class NestSampleViewModel : INotifyPropertyChanged {
 
 		public NestSampleViewModel() {
-			CurrentTemperature = "62";
-			TargetTemperature = "70";
+			CurrentTemperature = 68;
+			TargetTemperature = 68;
+			FanMode = FanMode.Auto;
+			HvacMode = HvacMode.CoolOnly;
 			IsLoggedIn = true;
 		}
 
-		private string _currentTemperature = "0";
-		public string CurrentTemperature {
+		private bool _isInErrorState = false;
+		public bool IsInErrorState {
+			get { return _isInErrorState; }
+			set {
+				_isInErrorState = value;
+				OnPropertyChanged("IsInErrorState");
+			}
+		}
+
+		private double _currentTemperature = 0;
+		public double CurrentTemperature {
 			get { return _currentTemperature; }
 			set {
 				_currentTemperature = value;
 				OnPropertyChanged("CurrentTemperature");
 			}
 		}
-		
-		private string _targetTemperature = "0";
-		public string TargetTemperature {
+
+		private double _targetTemperature = 0;
+		public double TargetTemperature {
 			get { return _targetTemperature; }
 			set {
 				_targetTemperature = value;
 				OnPropertyChanged("TargetTemperature");
+			}
+		}
+
+		private double _targetTemperatureLow;
+		public double TargetTemperatureLow {
+			get { return _targetTemperatureLow; }
+			set {
+				_targetTemperatureLow = value;
+				OnPropertyChanged("TargetTemperatureLow");
+			}
+		}
+
+		private double _targetTemperatureHigh;
+		public double TargetTemperatureHigh {
+			get { return _targetTemperatureHigh; }
+			set {
+				_targetTemperatureHigh = value;
+				OnPropertyChanged("TargetTemperatureHigh");
 			}
 		}
 
@@ -92,11 +121,41 @@ namespace WPNest {
 			}
 		}
 
+		private bool _isHeating;
+		public bool IsHeating {
+			get { return _isHeating; }
+			set {
+				_isHeating = value;
+				OnPropertyChanged("IsHeating");
+			}
+		}
+
+
+		private bool _isCooling;
+		public bool IsCooling {
+			get { return _isCooling; }
+			set {
+				_isCooling = value;
+				OnPropertyChanged("IsCooling");
+			}
+		}
+
+		private HvacMode _hvacMode;
+		public HvacMode HvacMode {
+			get { return _hvacMode; }
+			set {
+				_hvacMode = value;
+				OnPropertyChanged("HvacMode");
+			}
+		}
+
+
 		public event PropertyChangedEventHandler PropertyChanged;
 
 		protected virtual void OnPropertyChanged(string propertyName) {
 			PropertyChangedEventHandler handler = PropertyChanged;
-			if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+			if (handler != null)
+				handler(this, new PropertyChangedEventArgs(propertyName));
 		}
 	}
 }
